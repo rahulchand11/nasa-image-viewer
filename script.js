f6459f737087d44480935721a58ea083ca8b19b5
Rahul Chand <85656907+rahulchand11@users.noreply.github.com>
2024-05-05 09:21:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+document.addEventListener("click", function(e){
+    // console.log(e.target);
+    if(e.target.classList.contains("img-fluid")){
+        // console.log(e.target);
+        const src = e.target.getAttribute("src");
+        document.querySelector(".modal-img").src = src;
+        const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
+        myModal.show();
+     
+    }
+})
+
+
+// Nasa Image viewer
+const inputvals = document.getElementById('myInput');
+const imgchange = document.querySelector(".img-fluid");
+// const uservalue = inputvals.value;
+const container = document.querySelector(".container-fluid");
+const btn = document.querySelector(".input-group-text");
+const APIKEY = `BoRKVRlEQ9PT7SamJ1IKbVVGEomW3poChnvegMoY`;
+let video = document.createElement("iframe");
+            video.setAttribute("width", "900px");
+            video.setAttribute("height", "550px");
+
+btn.addEventListener("click", function(){
+   
+   const APIURL = `https://api.nasa.gov/planetary/apod?date=${inputvals.value}&api_key=${APIKEY}`
+   console.log(APIURL);
+         
+fetch(APIURL)
+.then((Response) => {
+    if (!Response.ok) {
+        console.log(APIURL);
+        console.log("Error fOunded", Response)
+    } else {
+        return Response.json();
+    }
+}).then((data) => {
+    container.appendChild(video);
+    imgchange.replaceWith(video);
+    console.log(data);
+    if (data.media_type === 'video') {
+           video.setAttribute("src", data.url);
+    }else if(data.media_type === 'image'){
+        container.replaceChild(imgchange, video);
+        imgchange.setAttribute("src", data.url);
+    }
+}).catch((error) => {
+    console.log("error founded", error);
+}).finally(() => {
+    console.log("runned")
+})
+});
